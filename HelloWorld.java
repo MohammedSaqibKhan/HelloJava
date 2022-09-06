5894a6d1fad451a594c0923cbdd3735240b4c15c
MohammedSaqib <mohammedsaqib1706@gmail.com>
2022-09-06 21:04:28 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public void sayHello() {
+        System.out.println("Hello world !! ");
+    }
+
+    public static void main(String[] args) {
+        HelloWorld world = new HelloWorld();
+        world.sayHello();
+    }
+}
